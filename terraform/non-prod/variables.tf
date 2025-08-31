7a1bf5537e040107a84d1254c241e9fed0916dbe
Connor Burge <connor.d.burge@gmail.com>
2025-08-31 19:56:16 -0400

    some working wip now=!

--- terraform/non-prod/variables.tf
+++ terraform/non-prod/variables.tf
@@ -10,21 +10,21 @@ variable "render_owner_id" {
 }
 
 variable "keycloak_admin_password" {
-  type = string
+  type        = string
   description = "keycloak_admin_password"
 }
 
 variable "kc_db_url" {
-  type = string
+  type        = string
   description = "kc_db_url"
 }
 
 variable "kc_db_username" {
-  type = string
+  type        = string
   description = "kc_db_username"
 }
 
 variable "kc_db_password" {
-  type = string
+  type        = string
   description = "kc_db_password"
 }
--- terraform/non-prod/web-service.tf
+++ terraform/non-prod/web-service.tf
@@ -12,13 +12,15 @@ resource "render_web_service" "keycloak_service_np" {
   }
 
   env_vars = {
-    "KEYCLOAK_ADMIN"     = { value = "admin" }
-    "KEYCLOAK_ADMIN_PASSWORD" = { value = var.keycloak_admin_password }
-    "KC_DB"              = { value = "postgres" }
-    "KC_DB_URL"          = { value = var.kc_db_url }
-    "KC_DB_USERNAME"     = { value = var.kc_db_username }
-    "KC_DB_PASSWORD"     = { value = var.kc_db_password }
-    "KC_HOSTNAME"        = { value = "keycloak-service-np.onrender.com" }
-    "KC_PROXY"           = { value = "edge" }
+    "KEYCLOAK_ADMIN"           = { value = "admin" }
+    "KEYCLOAK_ADMIN_PASSWORD"  = { value = var.keycloak_admin_password }
+    "KC_DB"                    = { value = "postgres" }
+    "KC_DB_URL"                = { value = var.kc_db_url }
+    "KC_DB_USERNAME"           = { value = var.kc_db_username }
+    "KC_DB_PASSWORD"           = { value = var.kc_db_password }
+    "KC_HOSTNAME"              = { value = "keycloak-service-np.onrender.com" }
+    "KC_PROXY"                 = { value = "edge" }
+    "KC_PROXY_HEADERS"         = { value = "xforwarded" }
+    "KC_HOSTNAME_STRICT_HTTPS" = { value = "false" }
   }
 }
