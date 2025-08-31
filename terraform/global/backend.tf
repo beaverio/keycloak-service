771e238279710ce1015a3ac57cba3115c83fab2f
Connor Burge <connor.d.burge@gmail.com>
2025-08-31 12:59:23 -0400

    some broken wip

--- terraform/global/backend.tf
+++ terraform/global/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "beaver-terraform-states"
-    key     = "keycloak-service/global/terraform.tfstate"
-    region  = "us-east-1"
+    bucket = "beaver-terraform-states"
+    key    = "keycloak-service/global/terraform.tfstate"
+    region = "us-east-1"
   }
 }
--- terraform/non-prod/main.tf
+++ terraform/non-prod/main.tf
@@ -1,8 +1,8 @@
 data "terraform_remote_state" "global" {
   backend = "s3"
   config = {
-    bucket  = "beaver-terraform-states"
-    key     = "keycloak-service/global/terraform.tfstate"
-    region  = "us-east-1"
+    bucket = "beaver-terraform-states"
+    key    = "keycloak-service/global/terraform.tfstate"
+    region = "us-east-1"
   }
 }
