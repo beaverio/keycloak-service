405c870163395ccc8aa7bb314b11f14c5f12e6b2
Connor Burge <connor.d.burge@gmail.com>
2025-08-31 20:05:03 -0400

    admin account chnage

--- terraform/non-prod/web-service.tf
+++ terraform/non-prod/web-service.tf
@@ -16,8 +16,8 @@ resource "render_web_service" "keycloak_service_np" {
     "KEYCLOAK_ADMIN_PASSWORD"  = { value = var.keycloak_admin_password }
     "KC_DB"                    = { value = "postgres" }
     "KC_DB_URL"                = { value = var.kc_db_url }
-    "KC_DB_USERNAME"           = { value = var.kc_db_username }
-    "KC_DB_PASSWORD"           = { value = var.kc_db_password }
+    "KC_DB_USERNAME"           = { value = render_postgres.keycloak_service_db_np.database_user }
+    "KC_DB_PASSWORD"           = { value = render_postgres.keycloak_service_db_np.connection_info.password }
     "KC_HOSTNAME"              = { value = "keycloak-service-np.onrender.com" }
     "KC_PROXY"                 = { value = "edge" }
     "KC_PROXY_HEADERS"         = { value = "xforwarded" }
