23888b2ed8bb07e1712b7f9d2fc4dbe3727ceacc
Connor Burge <connor.d.burge@gmail.com>
2025-09-02 17:06:06 -0400

    wired up the user profile metadata

--- terraform/keycloak/non-prod/realm.tf
+++ terraform/keycloak/non-prod/realm.tf
@@ -15,3 +15,118 @@ resource "keycloak_realm" "non_prod" {
   revoke_refresh_token        = true
   refresh_token_max_reuse     = 0
 }
+
+# User Profile Attributes
+resource "keycloak_realm_user_profile" "user_profile" {
+  realm_id = keycloak_realm.non_prod.id
+  unmanaged_attribute_policy = "ENABLED"
+
+  group {
+    name = "user-metadata"
+    display_header = "User Metadata"
+  }
+
+  attribute {
+    name = "username"
+    display_name = "username"
+    group = "user-metadata"
+    multi_valued = false
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+
+    validator {
+      name   = "length"
+      config = {
+        min: 3
+        max: 255
+      }
+    }
+    validator {
+      name   = "username-prohibited-characters"
+      config = {}
+    }
+    validator {
+      name   = "up-username-not-idn-homograph"
+      config = {}
+    }
+  }
+
+  attribute {
+    name = "email"
+    display_name = "email"
+    group = "user-metadata"
+    multi_valued = false
+    required_for_roles  = ["user"]
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+    validator {
+      name   = "email"
+      config = {}
+    }
+    validator {
+      name   = "length"
+      config = {
+        max: 255
+      }
+    }
+  }
+
+  attribute {
+    name = "firstName"
+    display_name = "firstName"
+    group = "user-metadata"
+    multi_valued = false
+    required_for_roles  = ["user"]
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+    validator {
+      name   = "length"
+      config = {
+        max: 255
+      }
+    }
+    validator {
+      name = "person-name-prohibited-characters"
+      config = {}
+    }
+  }
+
+  attribute {
+    name = "lastName"
+    display_name = "lastName"
+    group = "user-metadata"
+    multi_valued = false
+    required_for_roles  = ["user"]
+    permissions {
+      view = ["admin", "user"]
+      edit = ["admin", "user"]
+    }
+    validator {
+      name   = "length"
+      config = {
+        max: 255
+      }
+    }
+    validator {
+      name = "person-name-prohibited-characters"
+      config = {}
+    }
+  }
+
+  attribute {
+    name = "userId"
+    display_name = "userId"
+    group = "user-metadata"
+    multi_valued = false
+    permissions {
+      view = ["admin"]
+      edit = ["admin"]
+    }
+  }
+}
