294ed9d1f5e0cecba0eb64d28bd37ad1d6f8750c
Connor Burge <connor.d.burge@gmail.com>
2025-09-02 15:02:55 -0400

    formatting

--- terraform/keycloak/non-prod/realm.tf
+++ terraform/keycloak/non-prod/realm.tf
@@ -7,10 +7,10 @@ resource "keycloak_realm" "non_prod" {
   display_name = "Non-Production"
   enabled      = true
 
-  access_code_lifespan        = "900s" // 15 min
-  client_session_idle_timeout = "604800s" // 7 days
+  access_code_lifespan        = "900s"     // 15 min
+  client_session_idle_timeout = "604800s"  // 7 days
   client_session_max_lifespan = "2592000s" // 30 days
-  sso_session_idle_timeout    = "604800s" // 7 days
+  sso_session_idle_timeout    = "604800s"  // 7 days
   sso_session_max_lifespan    = "2592000s" // 30 days
   revoke_refresh_token        = true
   refresh_token_max_reuse     = 0
