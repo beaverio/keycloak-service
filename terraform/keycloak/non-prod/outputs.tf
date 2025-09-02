7de916a253ccb5157381f8ef747d94ad8c01e30c
Connor Burge <connor.d.burge@gmail.com>
2025-09-02 18:18:22 -0400

    outputting client secrets

--- terraform/keycloak/non-prod/outputs.tf
+++ terraform/keycloak/non-prod/outputs.tf
@@ -1,29 +1,21 @@
-// terraform client secrets
-output "terraform_client_id" {
-  value = keycloak_openid_client.terraform.client_id
+locals {
+  clients = {
+    terraform = {
+      client_id     = keycloak_openid_client.terraform.client_id
+      client_secret = keycloak_openid_client.terraform.client_secret
+    }
+    auth_gateway = {
+      client_id     = keycloak_openid_client.auth_gateway.client_id
+      client_secret = keycloak_openid_client.auth_gateway.client_secret
+    }
+    identity_service = {
+      client_id     = keycloak_openid_client.identity_service.client_id
+      client_secret = keycloak_openid_client.identity_service.client_secret
+    }
+  }
 }
 
-output "terraform_client_secret" {
-  value     = keycloak_openid_client.terraform.client_secret
-  sensitive = true
-}
-
-// auth-gateway client secrets
-output "auth_gateway_client_id" {
-  value = keycloak_openid_client.auth_gateway.client_id
-}
-
-output "auth_gateway_client_secret" {
-  value     = keycloak_openid_client.auth_gateway.client_secret
-  sensitive = true
-}
-
-// identity-service client secrets
-output "identity_service_client_id" {
-  value = keycloak_openid_client.identity_service.client_id
-}
-
-output "identity_service_client_secret" {
-  value     = keycloak_openid_client.identity_service.client_secret
+output "secrets" {
+  value     = local.clients
   sensitive = true
 }
